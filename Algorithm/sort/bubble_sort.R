388c46dfe7d1aff4c4669adf3ce4f294240224a5
yj <1306004786@qq.com>
2022-01-20 22:54:01 +0800

    .

--- Algorithm/sort/bubble_sort.R
+++ Algorithm/sort/bubble_sort.R
@@ -4,29 +4,24 @@ bubble_sort <- function(x) {
     if (n < 2) {
         return(x)
     }
-    i <- 1
-    flag <- TRUE
     count <- 0
-    while (i < n && flag) {
-        flag <- FALSE
-        if (!flag) {
-          for (j in 1:(n - i)) {
-              if (x[j] > x[j+1]) {
-                  temp <- x[j]
-                  x[j] <- x[j+1]
-                  x[j+1] <- temp
-                  flag <- TRUE
-                  count <- count + 1
-              }
-          }
-          if (!flag) {
+    for (i in 1:(n-1)) {
+        flag <- TRUE
+        for (j in 1:(n - i)) {
+            if (x[j] > x[j+1]) {
+                temp <- x[j]
+                x[j] <- x[j+1]
+                x[j+1] <- temp
+                flag <- FALSE
+                count <- count + 1
+            }
+        }
+        if (flag) {
             cat("共遍历", i, "趟,", "共交换", count, "次\n")
             return(x)
-          }
         }
-        i <- i + 1
     }
-    cat("共遍历", i-1, "趟 ", "共交换", count, "次\n")
+    cat("共遍历", i, "趟 ", "共交换", count, "次\n")
     return(x)
 }
 
